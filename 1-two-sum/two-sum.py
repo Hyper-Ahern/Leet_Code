0d72a8717cc3eb8eb6dc666f7bab56f6473925d3
Hyper Ahern <46539583+Hyper-Ahern@users.noreply.github.com>
2025-08-11 14:49:57 -0300

    Update two-sum.py
    clean code and reduce overhead

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-
-        solution_index_1 = -1
-        solution_index_2 = -1
 
         for index1, first_number in enumerate(nums):
                         
@@ -21,18 +12,4 @@ class Solution(object):
                 elif (first_number + second_number == target):
                     #The solution is found; Print out the indices
 
-                    solution_index_1 = index1
-                    solution_index_2 = index2
-                    
-                    solution = [solution_index_1, solution_index_2]
-                  
-                    # solution = "[" + str(solution_index_1) + ", " + str(solution_index_2) + "]"
-                    return(solution)
-
-                else:
-                    # Skip to the next iteration
-                    pass
-
-
-            
-
+                    return [index1, index2]  
