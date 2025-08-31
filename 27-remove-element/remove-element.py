2649def0c8fa15879eab8669d50a73d65b8e3f52
Hyper Ahern <hyper.ahern@dal.ca>
2025-08-30 22:00:01 -0300

    Time: 0 ms (100.00%) | Memory: 17.94 MB (11.10%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,11 +1,12 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         subtraction_counter = 0
-
+        
         for i in range(len(nums)):
             if nums[i] == val:
                 nums[i] = 101
                 subtraction_counter += 1
 
         nums.sort()
+        
         return len(nums) - subtraction_counter
